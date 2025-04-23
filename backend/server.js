5829b8719324df61f5532da9653b490cb54ffabe
SamuelSotomayor1 <andressotomayor477@gmail.com>
2025-04-23 17:21:01 -0400

    feat: server up

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('¡Servidor Fut433 funcionando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
